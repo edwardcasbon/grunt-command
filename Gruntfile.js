8c7eca069ecfa3c3bbbf72e5c52ab8f0d2445cee
Edward Casbon <edward@edwardcasbon.co.uk>
2016-05-09 16:44:40 +0100

    Grunt commands initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+/*
+* grunt-command
+* https://github.com/edwardcasbon/grunt-command
+*
+* Copyright (c) 2016 Edward Casbon
+* Licensed under the MIT license.
+*/
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        // Configuration.
+        command: {
+            options: {}
+        }
+
+    });
+
+    grunt.loadTasks('tasks');
+};
---
+++ tasks/command.js
@@ -0,0 +1,29 @@
+/*
+* grunt-command
+* https://github.com/edwardcasbon/grunt-command
+*
+* Copyright (c) 2016 Edward Casbon
+* Licensed under the MIT license.
+*/
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Multi task for running terminal commands.
+    grunt.registerMultiTask('command', 'Run terminal commands from Grunt', function() {
+        var exec = require('child_process').exec;
+        var cb = this.async();
+
+        exec(this.data.command, function(err, stdout, stderr) {
+            grunt.log.writeln(stdout);
+            if(stderr !== null) {
+                grunt.fail.warn(stderr);
+            }
+            cb();
+        });
+    });
+
+    // Register the default task.
+    grunt.registerTask('default', 'command');
+};
